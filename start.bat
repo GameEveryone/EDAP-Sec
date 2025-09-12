b6bf4a576216eafdbe9c81549b31149a4a524e37
GameEveryone <40837698+GameEveryone@users.noreply.github.com>
2025-09-11 20:06:51 -0400

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-
-echo Starting FastAPI server at http://127.0.0.1:8000 ...
-uvicorn decryption_api:app --reload --host 0.0.0.0 --port 8000
-pause
